2f51f7bbfce9a937c3fea975a8a4b732296d8173
John Paul Smithdeal <johnpaulsmithdeal@gmail.com>
2025-10-13 00:15:55 -0400

    added getsnips

--- models/http/base_http_models.py
+++ models/http/base_http_models.py
@@ -1,7 +1,19 @@
-from pydantic import BaseModel
+from datetime import datetime
 from sqlmodel import SQLModel
 
 class UserBase(SQLModel):
     email: str
     firstname: str
     lastname: str
+
+class SnipBase(SQLModel):
+    snipid: int
+    snipname: str
+    sniplanguage: str
+    snipdescription: str
+    lastmodified: datetime
+    snipshared: bool
+
+    #Need this pydantic nested Config class to allow object mapping on select
+    class Config:
+        orm_mode = True
